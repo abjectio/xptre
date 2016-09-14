18e85a3a357618b6db8d38c261df3cfc6a0dd4b0
abjectio <knut.erik@unlike.no>
2016-09-14 18:50:52 +0200

    buggy

--- firsttest_api.py
+++ firsttest_api.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
-from trello import TrelloApi
+from trello import TrelloApi, Boards
 from lib.util import populate_configs, initiate_logging, loginfo, shutdownLogger
-
-
 
 def main():
     """main function"""
@@ -13,7 +11,7 @@ def main():
 
     #Populate the configs
     parser_config = populate_configs()
-
+    
     #Config header name
     section = "config"
     AUTH_KEY = parser_config.get(section, 'AUTH_KEY')
@@ -22,9 +20,14 @@ def main():
 
     trello = TrelloApi(AUTH_KEY)
     token = trello.set_token(TOKEN)
-    your_board = trello.boards.get(BOARD_ID)
     
+    your_board = trello.boards.get(BOARD_ID)
+    my_lists = trello.boards.get_list(BOARD_ID)
 
+    print 'JOJO %s ' % my_lists
+
+
+    
 #############
 #Execute main
 if __name__ == '__main__':
