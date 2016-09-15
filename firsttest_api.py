14d9eaa81b1673de21bc3aa28ffa791d304c9561
abjectio <knut.erik@unlike.no>
2016-09-15 09:43:37 +0200

    The board is now complete with the list and cards

--- firsttest_api.py
+++ firsttest_api.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from trello import TrelloApi
+from trello import TrelloApi, Lists
 from lib.util import populate_configs, initiate_logging, loginfo, shutdownLogger
 
 
@@ -22,8 +22,29 @@ def main():
 
     trello = TrelloApi(AUTH_KEY)
     token = trello.set_token(TOKEN)
+
+
     your_board = trello.boards.get(BOARD_ID)
-    
+    your_board['lists'] = []
+    lists = trello.boards.get_list(BOARD_ID)
+    cards = trello.boards.get_card(BOARD_ID)
+
+
+    for one_list in lists:
+        id = one_list.get('id')
+        name = one_list.get('name')
+
+        cards_in_list = []
+        for one_card in cards:
+            if one_card.get('idList') == id:
+                cards_in_list.append(one_card)
+
+        one_list['cards'] = cards_in_list
+        your_board['lists'].append(one_list);
+
+
+    print ' Board = %s' % your_board
+
 
 #############
 #Execute main
