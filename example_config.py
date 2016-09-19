78c1520d2dcd60115a01dfad6c4e372965d1ffbc
abjectio <knut.erik@unlike.no>
2016-09-19 20:50:41 +0200

    updated

--- example_config.py
+++ example_config.py
@@ -1,11 +1,11 @@
 # Define the application directory
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DEBUG = True
+DEBUG = True/False #Debug mode in Flask
 
 # Trello
-AUTH_KEY='b879dd840b1861ede8faea97c283929d'
-TOKEN='81aa61354b6bc0b68f178451fdf8d530802979b75f166a8187c297fd1e9dc16e'
-BOARD_ID='57050707b13cf5c053dbc3a8'
-MEMBERS=True
-NO_DESCRIPTION=['Done']
+AUTH_KEY= '' #Trello auth key.
+TOKEN= '' #Trello token key.
+BOARD_ID= '' #id of the trello board you wish to visualise.
+MEMBERS= '' #True if you wish to show names on members of a card.
+NO_DESCRIPTION = '' #Array of strings (name on list), which should not show description in cards. E.g. a 'Done' column. Example: ['Done','ToDo / Backlog']
