fdb25c1a5383679eab885b03f8adead2ad8df6e0
abjectio <knut.erik@unlike.no>
2016-09-19 20:46:57 +0200

    Ohh another major restructure of app. Followed the example from https://www.digitalocean.com/community/tutorials/how-to-structure-large-flask-applications - app, modules, controllers, models.

---
+++ example_config.py
@@ -0,0 +1,11 @@
+# Define the application directory
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DEBUG = True
+
+# Trello
+AUTH_KEY='b879dd840b1861ede8faea97c283929d'
+TOKEN='81aa61354b6bc0b68f178451fdf8d530802979b75f166a8187c297fd1e9dc16e'
+BOARD_ID='57050707b13cf5c053dbc3a8'
+MEMBERS=True
+NO_DESCRIPTION=['Done']
